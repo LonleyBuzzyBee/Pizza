139d2380f39532c999a207e9f39aa8793dfd29cb
mai <mailiecook@yahoo.com>
2020-01-31 10:54:27 -0800

    add prototype pizzaCalc in scripts file, debug functionality in progress

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,30 @@
 //business logic
+function Pizza(pSize,pToppings,regPizza) {
+  this.pSize = pSize,
+  this.pToppings = pToppings,
+  this.regPizza = regPizza,
+  this.cost = 0
+}
+
+Pizza.prototype.pizzaCalc = function() {
+  if (pizza.pSize === "smallPizza"){
+      pizza.cost = 9
+    } if (pizza.pSize === "mediumPizza"){
+        pizza.cost = 12
+    } if (pizza.pSize === "largePizza"){
+        pizza.cost = 16
+    } if (pizza.pSize || pizza.pToppings ||pizza.regPizza === "none"){
+        pizza.cost += 0
+    } if (pizza.pToppings === "meat"){
+       pizza.cost += 4
+    } if (pizza.pToppings === "veggie"){
+       pizza.cost += 2
+    } if (pizza.pToppings === "cheese"){
+        pizza.cost += 1
+    } if (pizza.regPizza === "classic"){
+       pizza.cost = 13
+    }
+
 
 
 
@@ -13,9 +39,9 @@ $(document).ready(function() {
   $("#ticket").hide();
   $("form").submit(function(event) {
     event.preventDefault();
-    var p-size = $(".pizza-size").val();
-    var p-toppings = $(".toppings").val();
-    var r-pizza = $(".regular-pizzas").val();
+    var pizzaSize = $(".pizza-size").val();
+    var pizzaToppings = $(".toppings").val();
+    var regPizza = $(".regular-pizzas").val();
     var pizza = new Pizza(size, topping, regular);
   });
 });
