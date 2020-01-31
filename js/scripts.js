aab3a5540b0c60173077690722e3e54a638dc931
mai <mailiecook@yahoo.com>
2020-01-31 11:54:30 -0800

    debug functionality in scripts.js file, still in progress.

--- js/scripts.js
+++ js/scripts.js
@@ -7,41 +7,49 @@ function Pizza(pSize,pToppings,regPizza) {
 }
 
 Pizza.prototype.pizzaCalc = function() {
+  var price = addedFees();
+  return this.cost + price;
+  consol.log(pizzaCalc);
+}
+
+function checkStartPrice(pizza) {
   if (pizza.pSize === "smallPizza"){
-      pizza.cost = 9
-    } if (pizza.pSize === "mediumPizza"){
-        pizza.cost = 12
-    } if (pizza.pSize === "largePizza"){
-        pizza.cost = 16
-    } if (pizza.pSize || pizza.pToppings ||pizza.regPizza === "none"){
-        pizza.cost += 0
-    } if (pizza.pToppings === "meat"){
-       pizza.cost += 4
-    } if (pizza.pToppings === "veggie"){
-       pizza.cost += 2
-    } if (pizza.pToppings === "cheese"){
-        pizza.cost += 1
-    } if (pizza.regPizza === "classic"){
-       pizza.cost = 13
-    }
+    pizza.cost = 9
+  } if (pizza.pSize === "mediumPizza"){
+    pizza.cost = 12
+  } if (pizza.pSize === "largePizza"){
+    pizza.cost = 16
+  } if (pizza.regPizza === "classic"){
+    pizza.cost = 13
+  }
+  addedFees(pizza)
+};
+
+function addedFees(pizza) {
+  if (pizza.pSize || pizza.pToppings ||pizza.regPizza === "none"){
+    pizza.cost += 0
+  } if (pizza.pToppings === "meat"){
+    pizza.cost += 4
+  } if (pizza.pToppings === "veggie"){
+    pizza.cost += 2
+  } if (pizza.pToppings === "cheese"){
+    pizza.cost += 1
+  }
+
+  // pizza.prototype.pizzaCalc = function() {
 
 
-
-
-
-
-
-
-
-
-//user logic
-$(document).ready(function() {
-  $("#ticket").hide();
-  $("form").submit(function(event) {
-    event.preventDefault();
-    var pizzaSize = $(".pizza-size").val();
-    var pizzaToppings = $(".toppings").val();
-    var regPizza = $(".regular-pizzas").val();
-    var pizza = new Pizza(size, topping, regular);
+  var order = ""
+  //user logic
+  $(document).ready(function() {
+    $("form").submit(function(event) {
+      event.preventDefault();
+      var pizzaSize = $(".pizza-size").val();
+      var pizzaToppings = $(".toppings").val();
+      var regPizza = $(".regular-pizzas").val();
+      var pizza = new Pizza(size, topping, regular);
+      var fPrice = order.pizzaCalc();
+      $("#cost").text(fPrice);
+      checkStartPrice(pizza);
+    });
   });
-});
