44f40840dc98f18930ac6de532275d50f10339fa
mai <mailiecook@yahoo.com>
2020-01-31 16:59:04 -0800

    scripts.js debug in process. still not functioning

--- js/scripts.js
+++ js/scripts.js
@@ -42,33 +42,9 @@
   }
 
   Pizza.prototype.pizzaCalc = function() {
-    var price = addedFees();
-    return this.cost
+
   }
 
-  function checkStartPrice(pizza) {
-    if (pizza.pSize === "smallPizza"){
-      pizza.cost = 9
-    } if (pizza.pSize === "mediumPizza"){
-      pizza.cost = 12
-    } if (pizza.pSize === "largePizza"){
-      pizza.cost = 16
-    } if (pizza.regPizza === "classic"){
-      pizza.cost = 13
-    }
-    addedFees(pizza)
-  };
-
-  function addedFees(pizza) {
-    if (pizza.pSize || pizza.pToppings ||pizza.regPizza === "none"){
-      pizza.cost += 0
-    } if (pizza.pToppings === "meat"){
-      pizza.cost += 4
-    } if (pizza.pToppings === "veggie"){
-      pizza.cost += 2
-    } if (pizza.pToppings === "cheese"){
-      pizza.cost += 1
-    }
 
 
 
@@ -81,8 +57,35 @@
         var pizzaToppings = $(".toppings").val();
         var regPizza = $(".regular-pizzas").val();
         var pizza = new Pizza(size, topping, regular);
-        // var fPrice = order.pizzaCalc();
         $("#cost").text(fPrice);
         checkStartPrice(pizza);
       });
     });
+
+    // no bueno
+
+
+    // function checkStartPrice(pizza) {
+    //   if (pizza.pSize === "smallPizza"){
+    //     pizza.cost = 9
+    //   } if (pizza.pSize === "mediumPizza"){
+    //     pizza.cost = 12
+    //   } if (pizza.pSize === "largePizza"){
+    //     pizza.cost = 16
+    //   } if (pizza.regPizza === "classic"){
+    //     pizza.cost = 13
+    //   }
+    //   addedFees(pizza)
+    // };
+    //
+    // function addedFees(pizza) {
+    //   if (pizza.pSize || pizza.pToppings ||pizza.regPizza === "none"){
+    //     pizza.cost += 0
+    //   } if (pizza.pToppings === "meat"){
+    //     pizza.cost += 4
+    //   } if (pizza.pToppings === "veggie"){
+    //     pizza.cost += 2
+    //   } if (pizza.pToppings === "cheese"){
+    //     pizza.cost += 1
+    //   }
+    //
