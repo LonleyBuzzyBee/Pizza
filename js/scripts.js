1374442637b7864ee2cd8a682683875e64f2b7ba
mai <mailiecook@yahoo.com>
2020-02-04 23:14:18 -0800

    corrected spelling errors in html classes

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,11 @@
 
   //business logic
   function Pizza(pSize,mTopping,vTopping,cheeseTopping) {
-    this.pSize = pSize,
-    this.mTopping = mTopping,
-    this.vTopping = vTopping,
-    this.cheeseTopping = cheeseTopping,
-    this.cost = 0
+    this.pSize = pSize;
+    this.mTopping = mTopping;
+    this.vTopping = vTopping;
+    this.cheeseTopping = cheeseTopping;
+    this.cost = 0;
   }
 
   Pizza.prototype.startCost = function() {
@@ -45,62 +45,23 @@
       this.cost += 2
   }
 }
-// Pizza.prototype.addTotals = function() {
-//   var addedTotal = this.pSize +
-
-
-
 function printTotal(pizza) {
     console.log(pizza);
     $("#cost").text();
     $("#totalCost").html();
 }
-
-
     //user logic
-
     $(document).ready(function() {
       $("form").submit(function(event) {
         event.preventDefault();
         var pizzaSize = $(".pizza-size").val();
-        var meatTopping = $("meat-topping").val();
-        var vegetableTopping = $("vegetable-topping").val();
-        var cheeses = $("cheese").val();
+        var meatTopping = $(".meat-topping").val();
+        var vegetableTopping = $(".vegetable-topping").val();
+        var cheeses = $(".cheeses").val();
         var pizza = new Pizza(pizzaSize, meatTopping, vegetableTopping, cheeses);
         pizza.startCost();
         pizza.addMeat();
         pizza.addVeggie();
         pizza.addCheese();
-        // $("#cost").text();
-        printTotal(pizza);
-        // printTotal(piz;
       });
     });
-
-    // no bueno
-
-
-    // function checkStartPrice(pizza) {
-    //   if (pizza.pSize === "smallPizza"){
-    //     pizza.cost = 9
-    //   } if (pizza.pSize === "mediumPizza"){
-    //     pizza.cost = 12
-    //   } if (pizza.pSize === "largePizza"){
-    //     pizza.cost = 16
-    //   } if (pizza.regPizza === "classic"){
-    //     pizza.cost = 13
-    //   }
-    //   addedFees(pizza)
-    // };
-    //
-    // function addedFees(pizza) {
-    //   if (pizza.pSize || pizza.pToppings ||pizza.regPizza === "none"){
-    //     pizza.cost += 0
-    //   } if (pizza.pToppings === "meat"){
-    //     pizza.cost += 4
-    //   } if (pizza.pToppings === "veggie"){
-    //     pizza.cost += 2
-    //   } if (pizza.pToppings === "cheese"){
-    //     pizza.cost += 1
-    //   }
-    //
